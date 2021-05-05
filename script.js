5dfb294ad147a3c91505cc973e6db2c0b92df491
Vkar90 <kardamitsis.e@gmail.com>
2021-05-05 11:10:05 +0300

    'create object to store messages'

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //Testing local run with node
-console.log("Test message");
+// console.log("Test message");
+
+const messages = {
+  signInfo: ["star", "moon", "sun"],
+  fortuneOutput: [
+    "terrible luck",
+    "bad luck",
+    "ok luck",
+    "good luck",
+    "great luck"
+  ],
+  advice: ["go out", "do not read this", "play more", "trust no one"]
+};
